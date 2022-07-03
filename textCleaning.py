45fa47d5b396e4481ed7806080cdbb59f0a8ef96
domchang1 <dominicchang08@gmail.com>
2022-07-02 22:53:31 -0700

    basic text cleaning

--- textCleaning.py
+++ textCleaning.py
@@ -1,16 +1,35 @@
 import nltk
+from nltk.corpus import stopwords
+import re
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def textCleaning(line):
+    line.lower()
+    stop = stopwords.words('english')
+    line = " ".join([word for word in line.split() if word not in stop])
+    text = re.sub(r"(@\[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", line)
+    return text
+
+nltk.download('stopwords')
 dataset = pd.read_csv('twitter_data.csv', delimiter=',', encoding='latin-1')
 dataset.columns = ['target', 'ids', 'date', 'flag', 'user', 'text']
-dataset.plot(x='ids',y='target')
-plt.show()
-exit()
-shuffled_sample = dataset.sample(n=1000)
+dataset.drop('date', inplace=True, axis=1)
+dataset.drop('flag', inplace=True, axis=1)
+dataset.drop('user', inplace=True, axis=1)
+dataset.drop('ids', inplace=True, axis=1)
+dataset = dataset[['text', 'target']]
+#dataset.plot(x='ids',y='target')
+#plt.show()
 
-print(shuffled_sample.to_string())
-print(type(shuffled_sample))
+shuffled_sample = dataset.sample(n=10)
+shuffled_sample.to_dict()
+print(shuffled_sample)
+for index,row in shuffled_sample.iterrows():
+    print(textCleaning(row['text']))
+
+    
+
 #print(shuffled_sample.isin({'target': 2}))
 
 
