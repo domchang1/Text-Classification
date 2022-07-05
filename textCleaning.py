8cf15cb98edd95d9d8feecd1d965563f16d65bd0
domchang1 <dominicchang08@gmail.com>
2022-07-05 12:04:19 -0700

    trying tf-idf

--- textCleaning.py
+++ textCleaning.py
@@ -3,6 +3,7 @@ from nltk.corpus import stopwords
 import re
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def textCleaning(line):
     line.lower()
@@ -11,7 +12,7 @@ def textCleaning(line):
     text = re.sub(r"(@\[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", line)
     return text
 
-nltk.download('stopwords')
+#nltk.download('stopwords')
 dataset = pd.read_csv('twitter_data.csv', delimiter=',', encoding='latin-1')
 dataset.columns = ['target', 'ids', 'date', 'flag', 'user', 'text']
 dataset.drop('date', inplace=True, axis=1)
@@ -21,12 +22,20 @@ dataset.drop('ids', inplace=True, axis=1)
 dataset = dataset[['text', 'target']]
 #dataset.plot(x='ids',y='target')
 #plt.show()
-
 shuffled_sample = dataset.sample(n=10)
 shuffled_sample.to_dict()
 print(shuffled_sample)
 for index,row in shuffled_sample.iterrows():
     print(textCleaning(row['text']))
+
+tfIdfVectorizer= TfidfVectorizer(analyzer='word', stop_words='english')
+tfIdf = tfIdfVectorizer.fit_transform(list(shuffled_sample.values()))
+tokens = tfIdfVectorizer.get_feature_names_out()
+print(pd.DataFrame(data=tfIdf.toarray(), columns=tokens))
+exit()
+df = pd.DataFrame(tfIdf[0].T.todense(), index=tfIdfVectorizer.get_feature_names(), columns=["TF-IDF"])
+df = df.sort_values('TF-IDF', ascending=False)
+print (df.head(25))
 
     
 
