08fc9d2fd5ec695accfff3b4e4e751110527be07
domchang1 <dominicchang08@gmail.com>
2022-08-05 14:37:50 -0700

    basic machine learning model done

--- textCleaning.py
+++ textCleaning.py
@@ -1,18 +1,19 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-from nltk.stem import WordNetLemmatizer
 import re
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 import tensorflow as tf
 from tensorflow import keras
 import string
-
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+import sys
 def textCleaning(line):
     stemmer = PorterStemmer()
-    #lemmatizer = WordNetLemmatizer()
     line = line.lower()
     stop = set(stopwords.words('english'))
     line = " ".join([stemmer.stem(word) for word in line.split() if word not in stop])
@@ -37,42 +38,56 @@ dataset.drop('ids', inplace=True, axis=1)
 dataset = dataset[['text', 'target']]
 #dataset.plot(x='ids',y='target')
 #plt.show()
-shuffled_sample = dataset.sample(n=10)
+num_sample = 30000
+shuffled_sample = dataset.sample(n=num_sample)
 shuffled_sample = shuffled_sample.set_index('text').to_dict()
-print(shuffled_sample)
-# vectorize_layer = keras.layers.TextVectorization(
-#     standardize=textCleaning,
-#     max_tokens=1000,
-#     output_mode='int',
-#     output_sequence_length=250)
-# vectorize_layer.adapt(shuffled_sample)
-# exit()
 inputs = list(shuffled_sample['target'].keys())
 labels = list(shuffled_sample['target'].values())
 
-print(inputs)
-print(labels)
+# print(inputs)
+# print(labels)
 
 for index in range(len(inputs)):
-    print(textCleaning(inputs[index]))
+    # print(textCleaning(inputs[index]))
     inputs[index] = textCleaning(inputs[index])
-print(inputs)
+# print(inputs)
 
 #countvectorizer = CountVectorizer(analyzer= 'word', stop_words='english')
 tfIdfVectorizer= TfidfVectorizer(analyzer='word', stop_words='english')
 #count_wm = countvectorizer.fit_transform(inputs)
 tfIdf = tfIdfVectorizer.fit_transform(inputs)
 tokens = tfIdfVectorizer.get_feature_names_out()
-print(pd.DataFrame(data=tfIdf.toarray(), columns=tokens))
+data = pd.DataFrame(data=tfIdf.toarray(), columns=tokens)
+# data['label'] = labels
+# print(data)
+# data = data.values.tolist()
+# data = np.stack(data)
+x = data.to_numpy()
+print(data.shape)
+y = np.stack(labels)
+y[y > 1] = 1
+# np.set_printoptions(threshold=sys.maxsize)
+# print(y)
+# exit()
+# print(x.shape)
+# print(y.shape)
+# exit()
+
+train_inp = x[:int(num_sample*0.8)]
+train_label = y[:int(num_sample*0.8)]
+valid_inp = x[int(num_sample*0.8):]
+valid_label = y[int(num_sample*0.8):]
+# print(train_inp.shape)
+# print(valid_label)
+model = RandomForestClassifier(max_depth = 20, n_estimators= 500)
+model.fit(train_inp, train_label)
+predictions = model.predict(valid_inp)
+results = (predictions == valid_label)
+print(results.astype(int).mean())
+
+
+
+#train test split, scikit learn random forest classifier
 exit()
-df = pd.DataFrame(tfIdf[0].T.todense(), index=tfIdfVectorizer.get_feature_names(), columns=["TF-IDF"])
-df = df.sort_values('TF-IDF', ascending=False)
-print (df.head(25))
-
-    
-
-#print(shuffled_sample.isin({'target': 2}))
-
-
 
 #venv\Scripts\activate.bat to activate venv
